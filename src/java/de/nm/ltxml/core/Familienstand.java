e36c01d6b091a48b847652543ac03c8fa3df4459
lukashecke <57808594+lukashecke@users.noreply.github.com>
2021-07-07 10:58:12 +0200

    Test 1 (TDD)

---
+++ src/java/de/nm/ltxml/core/Familienstand.java
@@ -0,0 +1,16 @@
+package de.nm.ltxml.core;
+
+public class Familienstand extends Bezeichnung {
+    public Familienstand() {
+        super();
+    }
+
+    public Familienstand(String id, String bezeichnung) {
+        super(id, bezeichnung);
+    }
+
+    @Override
+    public String toString() {
+        return "Familienstand: id="+this.id+", bezeichnung="+this.bezeichnung+"";
+    }
+}
